d17bac2dcd2a7b932d3f4e9c8fb8a1442fbfa6f6
Ali Waseem <waseema393@gmail.com>
2016-07-23 20:48:20 -0700

    Added header component

--- src/container/Header/header.js
+++ src/container/Header/header.js
@@ -2,18 +2,30 @@ import React from 'react';
 import {
   Header as JamHeader,
   H1,
+  TextCenter,
+  H5,
+  H6,
+  Button,
 } from 'jam-components';
 
-const HEADER_IMAGE = 'http://66.media.tumblr.com/a734da7b5eee3e683179760f95c00342/tumblr_oaqg7qn30y1tq11emo1_1280.jpg';
-const HEADER_HEIGHT = '65vh';
+const HEADER_IMAGE = 'https://images.unsplash.com/photo-1445089073948-809762476ed5?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&s=dd6c15997d6cf1dec40c7426acc9768d';
+const HEADER_HEIGHT = '75vh';
+const BUTTON_COLOR = 'black';
+const HEADER_PADDING_TOP = '225px';
 
 function Header() {
   return (
     <JamHeader
       image={HEADER_IMAGE}
       height={HEADER_HEIGHT}
+      paddingTop={HEADER_PADDING_TOP}
     >
-      <H1 align="center">Ali Waseem</H1>
+      <TextCenter>
+        <H1>Ali Waseem</H1>
+        <H5>Software Engineer, Proud Canadian</H5>
+        <H6>Living in Los Angeles, CA</H6>
+        <Button color={BUTTON_COLOR}>tell me more</Button>
+      </TextCenter>
     </JamHeader>
   );
 }
